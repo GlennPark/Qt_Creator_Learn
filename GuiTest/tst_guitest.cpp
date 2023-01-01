71973f93b5ca638fe195a2c962fadb7cd8bc0f88
glennpark <downgear@naver.com>
2023-01-01 19:52:19 +0900

    0101_01
    실습01

--- GuiTest/tst_guitest.cpp
+++ GuiTest/tst_guitest.cpp
@@ -10,9 +10,11 @@ class GuiTest : public QObject
 public:
     GuiTest();
     ~GuiTest();
+    QLineEdit* lineEdit;
 
 private slots:
     void test_case1();
+    void test_case1_data();
 
 };
 
@@ -25,13 +27,51 @@ GuiTest::~GuiTest()
 {
 
 }
+void GuiTest::test_case1_data( )
+{
+    QTest::addColumn<QTestEventList>("events");
+    QTest::addColumn<QString>("expected");
+
+    QTestEventList list1;
+    list1.addKeyClick('a');
+    QTest::newRow("char") << list1 << "a";
+
+    QTestEventList list2;
+    list2.addKeyClick('a');
+    list2.addKeyClick(Qt::Key_Backspace);
+    QTest::newRow("there+back-again") << list2 << "";
+}
 
 void GuiTest::test_case1()
 {
-    QLineEdit lineEdit;
-    QTest::keyClicks(&lineEdit,"hi KDE");
-    QCOMPARE(lineEdit.text(), "hi KDE");
-    QVERIFY2(true, "Failure");
+
+
+    QFETCH(QTestEventList, events);
+        QFETCH(QString, expected);
+
+        events.simulate(lineEdit);
+
+        QCOMPARE(lineEdit->text(), expected);
+
+//    QLineEdit lineEdit;
+//    QTest::keyClicks(&lineEdit,"hi KDE");
+//    QCOMPARE(lineEdit.text(), "hi KDE");
+//    QVERIFY2(true, "Failure");
+
+    QTest::keyClicks(lineEdit, "hi KDE");
+
+//   QTestEventList events;
+    events.addKeyClick('a');
+    events.addKeyClick(Qt::Key_Backspace);
+    events.addDelay(200);
+
+//    QLineEdit *lineEdit = new QLineEdit(myParent);
+
+    events.simulate(lineEdit);
+    QCOMPARE(lineEdit->text(),"");
+    events.simulate(lineEdit);
+    QCOMPARE(lineEdit->text()," ");
+
 }
 
 QTEST_MAIN(GuiTest)
